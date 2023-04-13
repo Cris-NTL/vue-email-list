7bf39289ff0e1709576709fc7bc8dbcb37717257
Cris-NTL <natolicristian92@gmail.com>
2023-04-13 16:26:30 +0200

    Add: Random Email

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            random_mail_url: 'https://flynn.boolean.careers/exercises/api/random/mail',
+            random_mail: [],
+
+        }
+    },
+    mounted() {
+
+        for (let index = 0; index < 10; index++) {
+            axios
+                .get(this.random_mail_url)
+                .then(response => {
+                    this.random_mail.push(response.data.response)
+                });
+        }
+
+    }
+}).mount('#app')
